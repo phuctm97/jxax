07cbe3d606f58609058ab510e54c52c28b835253
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-04 15:44:30 +0700

    feat(devtool): add dev scripting model

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import run from 'jxax/examples/main';
+import enableDevtool from 'jxax/core/devtool';
 
-run();
+let devtool;
+if (process.env.NODE_ENV === 'development') {
+  devtool = enableDevtool({
+    'Application.currentApplication().read': () => JSON.stringify({}),
+  });
+}
+
+const fn = require('jxax/examples/main').default;
+
+fn();
+
+if (devtool) {
+  console.log(devtool.toString());
+}
