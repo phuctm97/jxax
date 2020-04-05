3bfaa9bd29aabc77665b611f14327639bcb28459
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-05 10:47:25 +0700

    chore(devtool): only enable devtool on node target and improve log readabilty

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import enableDevtool from 'jxax/core/devtool';
+import colors from 'ansi-colors';
 
 let devtool;
-if (process.env.NODE_ENV === 'development') {
+if (process.env.TARGET_ENV === 'node') {
   devtool = enableDevtool({
     'Application.currentApplication().read': () => JSON.stringify({}),
+    ['Application("System Events")'
+    + '.applicationProcesses[Application("System Preferences").name()]'
+    + '.windows[0].popUpButtons.whose({"description":"Recent Items"})[0].value']: 'None',
   });
 }
 
@@ -12,5 +16,6 @@ const fn = require('jxax/examples/main').default;
 fn();
 
 if (devtool) {
-  console.log(devtool.toString());
+  console.log(colors.bold.whiteBright('⛓Scripting model execution:'));
+  console.log(colors.dim(devtool));
 }
