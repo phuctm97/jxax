a44d6754ce5be9df3055c3771126fae8b09b085d
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-07 19:53:31 +0700

    refactor(devtool): drop devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,20 @@
 const path = require('path');
 
-const { DefinePlugin } = require('webpack');
-
 function isDev(env) {
-  // Have to explicitly specify env to set to development mode, production mode is set by default.
   return env && env.dev;
 }
 
-const Targets = {
-  NODE: 'node',
-  OSA: 'osa',
-};
-
-function isTargetNode(env) {
-  // Have to explicitly specify env to target node.
-  return env && (env.target === Targets.NODE || env[Targets.NODE]);
-}
-
-function isTargetOSA(env) {
-  // Target OSA by default.
-  return !env || (env.target === Targets.OSA || env[Targets.OSA]);
-}
-
-function config(target, env) {
-  return {
-    mode: isDev(env) ? 'development' : 'production',
-    output: {
-      filename: `main.${target}.js`,
+module.exports = (env) => ({
+  mode: isDev(env) ? 'development' : 'production',
+  resolve: {
+    alias: {
+      jxax: path.resolve(__dirname, 'src'),
     },
-    resolve: {
-      alias: {
-        jxax: path.resolve(__dirname, 'src'),
-      },
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      ],
-    },
-    plugins: [
-      new DefinePlugin({
-        'process.env.TARGET_ENV': JSON.stringify(target),
-      }),
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
     ],
-    // Don't generate source-map for OSA target as osascript doesn't support source-map (yet).
-    devtool: (isDev(env) && target !== Targets.OSA) ? 'source-map' : false,
-  };
-}
-
-module.exports = (env) => {
-  const configs = [];
-  if (isTargetNode(env)) configs.push(config(Targets.NODE, env));
-  if (isTargetOSA(env)) configs.push(config(Targets.OSA, env));
-  return configs;
-};
+  },
+  devtool: isDev(env) ? 'source-map' : false,
+});
