6ee2775be176a37e57353179a8e0776272081e19
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-10 15:16:22 +0700

    refactor(core): remove archieved modules

--- src/archived/test/getConfig.js
+++
@@ -1,9 +0,0 @@
-import { readFile, chooseFile } from 'jxax/core/addtions';
-
-export default function getConfig() {
-  const jsonFile = chooseFile({
-    withPrompt: 'Choose your JSON configuration file',
-    ofType: ['public.json'],
-  });
-  return JSON.parse(readFile(jsonFile));
-}
