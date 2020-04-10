0995ff99cdc2c829326124c0ace32710fc9a0258
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-10 12:00:02 +0700

    feat(core): add ui automation tools

--- src/util/index.js
+++ src/util/index.js
@@ -1,2 +1,3 @@
+export { default as isDevelopment } from 'jxax/util/isDevelopment';
 export { default as isPromise } from 'jxax/util/isPromise';
 export { default as nameOf } from 'jxax/util/nameOf';
---
+++ src/util/isDevelopment.js
@@ -0,0 +1,3 @@
+export default function isDevelopment() {
+  return process.env.NODE_ENV === 'development';
+}
