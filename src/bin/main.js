b05699efb0dd9f991b72d6733a4dcb2f163d1116
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-16 07:53:51 +0700

    refactor(core): move workflow config and load to @core/workflow module

--- src/bin/main.js
+++ src/bin/main.js
@@ -1,35 +1,8 @@
-import { has } from 'lodash';
-import runWorkflow, { createJob } from '@core/workflow';
+import runWorkflow, { loadJobs } from '@core/workflow';
 import createConsoleReporter from '@reporters/console';
 import readConfig from '@bin/readConfig';
 import library from '@bin/library';
 
-/**
- * @typedef {import('@bin/readConfig').WorkflowConfig}
- * @typedef {import('@core/workflow').Job}
- */
-
-/**
- * Load jobs for a workflow based on its configuration object.
- *
- * @param {WorkflowConfig} config The workflow configuration object.
- * @returns {Job[]} The workflow's jobs.
- */
-function loadJobs(config) {
-  const jobs = [];
-
-  config.jobs.forEach((jobConfig) => {
-    const { uses, args } = jobConfig;
-    if (!has(library, uses)) {
-      throw new Error(`unknown action '${uses}'`);
-    }
-
-    const action = library[uses];
-    jobs.push(createJob(uses, action.validate, action.run, args));
-  });
-
-  return jobs;
-}
 
 /**
  * The `jxax` CLI application entry.
@@ -40,7 +13,7 @@ export default function main(args) {
   if (args.length !== 1) throw new Error('expected [file] argument');
 
   const config = readConfig(args[0]);
-  const jobs = loadJobs(config);
+  const jobs = loadJobs(library, config);
   const reporter = createConsoleReporter();
   if (!runWorkflow(jobs, { reporter })) {
     throw new Error('workflow failed');
