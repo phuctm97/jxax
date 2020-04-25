2e718cb2bd5c093153255a741c668f11acdec02e
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-25 17:08:54 +0700

    chore: refactor order of commands in library, update package.json version

--- src/bin/library.js
+++ src/bin/library.js
@@ -5,12 +5,12 @@ import * as sysprefs from '@apps/sysprefs';
  * The `jxax` CLI's library of `Command`(s).
  */
 const library = {
+  'desktops.changePicture': desktops.changePicture,
+  'desktops.configureScreenSaver': desktops.configureScreenSaver,
   'sysprefs.configureGeneral': sysprefs.configureGeneral,
   'sysprefs.configureDock': sysprefs.configureDock,
   'sysprefs.configureMissionControl': sysprefs.configureMissionControl,
   'sysprefs.configureSpotlight': sysprefs.configureSpotlight,
-  'desktops.changePicture': desktops.changePicture,
-  'desktops.configureScreenSaver': desktops.configureScreenSaver,
 };
 
 export default library;
