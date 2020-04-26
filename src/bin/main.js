e6743938072ff9f2dd3cc8b6c900d15e27459218
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2020-04-26 14:58:55 +0700

    feat: CLI options `simple` and `no-color`

--- src/bin/main.js
+++ src/bin/main.js
@@ -1,8 +1,17 @@
+import * as getopts from 'getopts';
 import runWorkflow, { loadJobs } from '@core/workflow';
 import createConsoleReporter from '@reporters/console';
 import readConfig from '@bin/readConfig';
 import library from '@bin/library';
 
+/**
+ * The `jxax` CLI usage.
+ */
+const USAGE = `
+usage: jxax WORKFLOW_FILE [-s|--simple] [--no-color]
+  -s, --simple     Print in simple format
+    --no-color     Print in plain format
+`.trim();
 
 /**
  * The `jxax` CLI application entry.
@@ -10,11 +19,30 @@ import library from '@bin/library';
  * @param {string[]} args The CLI arguments.
  */
 export default function main(args) {
-  if (args.length !== 1) throw new Error('expected [file] argument');
+  // Parse args.
+  const opts = getopts(args, {
+    alias: {
+      help: 'h',
+      simple: 's',
+    },
+    default: {
+      color: true,
+      simple: false,
+    },
+  });
 
+  if (opts.help) { // Print help.
+    console.log(`jxax CLI\n\n${USAGE}`);
+    return;
+  }
+
+  const file = opts._[0];
+  if (!file) throw new Error(`expected WORKFLOW_FILE argument\n\n${USAGE}`);
+
+  // Run workflow.
   const config = readConfig(args[0]);
   const jobs = loadJobs(library, config);
-  const reporter = createConsoleReporter();
+  const reporter = createConsoleReporter({ simple: opts.simple, color: opts.color });
   if (!runWorkflow(jobs, { reporter })) {
     throw new Error('workflow failed');
   }
